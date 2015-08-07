136bf662f5fd180cae72f78801333d561413dc27
flaviobarros <flaviomargarito@gmail.com>
2015-08-06 21:19:37 -0300

    Exemplo de consulta.

--- explorations.R
+++ explorations.R
@@ -8,5 +8,12 @@ load('escolas.rda')
 horacio = filter(escolas, grepl(pattern = 'HORACIO AUGUSTO', x=escolas$'NOME DA ENTIDADE'))
 
 ## Obtendo as médias
-medias = horacio[, grepl(pattern = 'MÉDIA -', x = colnames(horacio))]
-barplot(as.matrix(horacio[, grepl(pattern='MÉDIA -', x = colnames(horacio))]))
+medias = horacio[, grepl(pattern = 'MÉDIA -', 
+                         x = colnames(horacio))]
+barplot(as.matrix(horacio[, grepl(pattern='MÉDIA -', 
+                                  x = colnames(horacio))]))
+
+## Exemplo de consulta: Indicador ENEM2014, UF = SP
+resultado = select(escolas, escola, UF, cidade, dep_administrativa, ENEM_2014) %>%
+            filter(UF == 'SP') %>%
+            arrange(desc(ENEM_2014))
